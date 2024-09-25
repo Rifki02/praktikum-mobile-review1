44f15a74edd573cb1ec2b98ee5a942e680c1bc42
Rifki02 <rifki.tosca2004@gmail.com>
2024-09-25 10:56:01 +0800

    Review2

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Praktikum'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -23,70 +24,122 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
+    final titles = ['Bike', 'Boat', 'Bus', 'car'];
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text("List View"),
+        backgroundColor: Colors.deepOrange[300],
       ),
-      body: Center(
-        child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            width: 100,
-            height: 100,
-            margin: EdgeInsets.only(bottom: 30),
-            color: Colors.red,
-          ),
-          Container(
-            width: 100,
-            height: 100,
-            margin: EdgeInsets.only(bottom: 30),
-            color: Colors.blue,
-            child: Column(
-              children: [
-                Container(
-            width: 100,
-            height: 100,
-            margin: EdgeInsets.only(right: 100),
-            color: Colors.green,
-
+      body: ListView.builder(
+        itemCount: titles.length,
+        itemBuilder: (context, nomor) {
+          return Card(
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage("http://picsum.photos/200/300?Images=$nomor"),
+              ),
+              title: Text(titles[nomor]),
             ),
-              ],
-            ),
-            ),
-          Container(
-            width: 100,
-            height: 100,
-            margin: EdgeInsets.only(bottom: 30),
-            color: Colors.green,
-            
-            ),
-             Container(
-            width: 100,
-            height: 100,
-            margin: EdgeInsets.only(bottom: 30),
-            color: Colors.red,
-          ),
-        ],
-            ),
+          );
+        },
       ),
     );
   }
-}
+}      // ListView(
+      //   // children: [
+      //   //   ListTile(
+      //   //     // leading: Icon(Icons.sunny)
+      //   //     leading: CircleAvatar(
+      //   //       backgroundImage: AssetImage("matahari.jpg"),
+      //   //     ),
+      //   //     title: Text("Sun"),
+      //   //     trailing: Icon(Icons.keyboard_arrow_right),
+      //   //   ),
+      //   //   ListTile(
+      //   //     leading: Icon(Icons.brightness_3),
+      //   //     title: Text("Moon"),
+      //   //     trailing: Icon(Icons.keyboard_arrow_right),
+      //   //   ),
+      //   //   ListTile(
+      //   //     leading: Icon(Icons.star),
+      //   //     title: Text("Starx"),
+      //   //     trailing: Icon(Icons.keyboard_arrow_right),
+      //   //   ),
+      //   // ],
+      // )
+      // Center(
+      // ListView(
+      //   children: [
+      //     Container(
+      //     width: 100,
+      //     height: 250,
+      //     margin: EdgeInsets.only(bottom: 10),
+      //     color: Colors.red,
+      //     ),
+      //     Container(
+      //     width: 100,
+      //     height: 250,
+      //     margin: EdgeInsets.only(bottom: 10),
+      //     color: Colors.blue,
+      //     ),
+      //     Container(
+      //     width: 100,
+      //     height: 250,
+      //     margin: EdgeInsets.only(bottom: 10),
+      //     color: Colors.green,
+      //     ),
+      //   ],
+      // ),
+      // GridView(
+      //   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+      //     crossAxisCount: 4,
+      //   ),
+      //   children: [
+      //     Container(
+      //     width: 100,
+      //     height: 250,
+      //     margin: EdgeInsets.only(bottom: 10),
+      //     // color: Colors.red,
+      //     decoration:const BoxDecoration(
+      //       // color: Colors.red,
+      //       image: DecorationImage(
+      //         image: AssetImage("jet.png"))          ),
+
+      //     ),
+      //     Container(
+      //     width: 100,
+      //     height: 250,
+      //     margin: EdgeInsets.only(bottom: 10),
+      //     color: Colors.blue,
+      //     ),
+      //     Container(
+      //     width: 100,
+      //     height: 250,
+      //     margin: EdgeInsets.only(bottom: 10),
+      //     color: Colors.green,
+      //     ),
+      //     Container(
+      //     width: 100,
+      //     height: 250,
+      //     margin: EdgeInsets.only(bottom: 10),
+      //     color: Colors.black,
+      //     ),
+      //   ],
+      // ),
+    //   child: Container(
+    //     width: 200,
+    //     height: 200,
+    //     decoration: const BoxDecoration(
+    //     image: DecorationImage(
+    //     image: AssetImage("assets/jet.png")
+    //     ),
+    //   ),
+    //   ),
+    // ),
